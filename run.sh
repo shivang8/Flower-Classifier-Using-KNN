3aeca303c3c44a3a044a0c712addfd5cb9060eec
shivang8 <satyamsri8@gmail.com>
2018-06-05 16:46:13 +0530

    Shell Script Added

---
+++ run.sh
@@ -0,0 +1,7 @@
+python main.py iris.csv 3
+python main.py iris.csv 5
+python main.py iris.csv 7
+python main.py iris.csv 11
+python main.py iris.csv 13
+python main.py iris.csv 17
+python main.py iris.csv 19
